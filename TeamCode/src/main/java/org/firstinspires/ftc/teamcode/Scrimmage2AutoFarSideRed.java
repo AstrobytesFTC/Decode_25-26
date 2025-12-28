8a32aff61543b6ee5adb28bf74a92b154a3eedc2
Mugilshark <94998254+Mugilshark@users.noreply.github.com>
2025-12-28 10:36:40 -0500

    Scrimmage2AutoFarSideRed change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Scrimmage2AutoFarSideRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Scrimmage2AutoFarSideRed.java
@@ -15,6 +15,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 @Config
@@ -22,209 +23,149 @@ import com.acmerobotics.roadrunner.Pose2d;
 public class Scrimmage2AutoFarSideRed extends LinearOpMode {
 
 
-    DcMotor lf = null;
-    DcMotor lb = null;
-    DcMotor rf = null;
-    DcMotor rb = null;
+    DcMotor frontLeftMotor = null;
+    DcMotor backLeftMotor = null;
+    DcMotor frontRightMotor = null;
+    DcMotor backRightMotor = null;
 
     DcMotor transfer =null;
 
-    DcMotor launcher = null;
+    // DcMotor launcher = null;
     DcMotor intake = null;
+
+    DcMotor shooterRight = null;
+    DcMotor shooterLeft = null;
+    Servo blocker = null;
 
     // lift class
     private boolean initialized = false;
 
-    public class warmupLaunch implements InstantFunction{
-        @Override
-        public void run(){
-            launcher.setPower(-0.8);
-        }
+//    public class warmupLaunch implements InstantFunction{
+//        @Override
+//        public void run(){
+//            launcher.setPower(-0.8);
+//        }
+//    }
+
+//    public class reverselaunch implements InstantFunction{
+//        @Override
+//        public void run(){
+//            launcher.setPower(0.5);
+//        }
+//    }
+//    public class stopLauncher implements InstantFunction{
+//        @Override
+//        public void run(){
+//            launcher.setPower(0);
+//        }
+//    }
+
+    public void timeTransferAndIntake(double seconds) {
+        transfer.setPower(0.9);
+        intake.setPower(-0.8);
+        sleep(Math.round(seconds * 1000)); // seconds → ms
+        transfer.setPower(0);
+        intake.setPower(0);
     }
 
-    public class reverselaunch implements InstantFunction{
+    public class functionOfDOOM implements InstantFunction{
         @Override
         public void run(){
-            launcher.setPower(0.5);
+            blocker.setPosition(0.5);
+            // === ULTIMATE FUNCTION OF DOOM ===
+            shooterRight.setPower(-0.90);
+            shooterLeft.setPower(0.90);
+
+// Spin-up time
+            sleep(2000);
+
+// === 1st Ball ===
+            timeTransferAndIntake(0.15);
+            sleep(1500);
+
+// === 2nd Ball ===
+            timeTransferAndIntake(0.25);
+            sleep(1500);
+
+// === 3rd Ball ===
+            timeTransferAndIntake(0.35);
+            sleep(1500);
+
+// Power down shooter
+            shooterLeft.setPower(0);
+            shooterRight.setPower(0);
+
+            blocker.setPosition(0);
         }
     }
-    public class stopLauncher implements InstantFunction{
+    public class smartIntake implements InstantFunction{
         @Override
         public void run(){
-            launcher.setPower(0);
+            //Change If needed
+            intake.setPower(-0.9);
+            transfer.setPower(0.9);
         }
     }
-
-    public class transferArtifact implements InstantFunction{
+    public class stopSmartIntake implements InstantFunction{
         @Override
         public void run(){
-            transfer.setPower(-1);
-
-        }
-    }
-
-    public class reverseTransferArtifact implements InstantFunction{
-        @Override
-        public void run(){
-            transfer.setPower(0.5);
-            sleep(60);
-
-        }
-    }
-
-    public class intakeFeed implements InstantFunction{
-        @Override
-        public void run(){
-            intake.setPower(1);
-
-
-        }
-    }
-    public class stopintake implements InstantFunction{
-        @Override
-        public void run(){
+            //Change If needed
             intake.setPower(0);
-
-        }
-    }
-
-    public class Shoot implements InstantFunction{
-        @Override
-        public void run(){
-
-            transfer.setPower(-1);
-            sleep(600);
             transfer.setPower(0);
-        }
-    }
-    public class slowNSteady implements InstantFunction{
-        @Override
-        public void run(){
-            intake.setPower(0.2);
-
         }
     }
 
 
 
     public void runOpMode() {
-        transfer = hardwareMap.dcMotor.get("transfer");
-        launcher = hardwareMap.dcMotor.get("launcher");
-        intake = hardwareMap.dcMotor.get("intake");
-        Pose2d beginPose = new Pose2d(new Vector2d(60,16), Math.toRadians(-45));
-        //this pose assumes the robot starts with the intake acing away from the goal. the shooter will be facing away from the goal
+
+
+        frontLeftMotor = hardwareMap.dcMotor.get("frontleft");
+        backLeftMotor  = hardwareMap.dcMotor.get("backleft");
+        frontRightMotor = hardwareMap.dcMotor.get("frontright");
+        backRightMotor  = hardwareMap.dcMotor.get("backright");
+        transfer = hardwareMap.dcMotor.get("intake");
+        intake = hardwareMap.dcMotor.get("transfer");
+        shooterLeft = hardwareMap.dcMotor.get("rightShooter");
+        shooterRight = hardwareMap.dcMotor.get("leftShooter");
+        blocker = hardwareMap.servo.get("blocker");
+
+        Pose2d beginPose = new Pose2d(new Vector2d(56,12), Math.toRadians(0));
+        //this pose assumes the robot starts with the intake facing away from the goal. the shooter will be facing away from the goal
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
 
         waitForStart();
 
         //creating RR path
-
-        // actionBuilder builds from the drive steps passed to it
-        //this path moves backwards and turns
-        Action path = drive.actionBuilder(beginPose)
-                .stopAndAdd(new slowNSteady())
-                .lineToX(-25)
-                .stopAndAdd(new warmupLaunch())
-                .turn(Math.toRadians(-125))
-                .stopAndAdd(new Shoot())
-                .waitSeconds(1)
-                .lineToX(-34)
-                .stopAndAdd(new intakeFeed())
-                .stopAndAdd(new transferArtifact())
-                .waitSeconds(3)
-                .turn(Math.toRadians(45))
-                .stopAndAdd(new stopLauncher())
-                .stopAndAdd(new reverselaunch())
-                .waitSeconds(1)
-                .strafeTo(new Vector2d(-12,-22))
-                .waitSeconds(1)
-                .strafeTo(new Vector2d(-12,-33))
-                .strafeTo(new Vector2d(-12,-22))
-                .stopAndAdd(new stopintake())
-                .strafeTo(new Vector2d(-52,-12))
-                .turn(Math.toRadians(-30))
+        Action complexfar = drive.actionBuilder(beginPose)
+//                .strafeToLinearHeading(new Vector2d(40,12),Math.toRadians(170))
+//                .strafeTo(new Vector2d(56,12))
+////                .turn(Math.toRadians(-80))
+////                .strafeTo(new Vector2d(35,12))
+////                .strafeTo(new Vector2d(35,50))
+////                .strafeToLinearHeading(new Vector2d(35,12),Math.toRadians(170))
+////                .strafeTo(new Vector2d(56,12))
+////                .strafeTo(new Vector2d(56,35))
                 .build();
-        Action path2 = drive.actionBuilder(beginPose)
-                .lineToX(-25)
-                .turn(Math.toRadians(-130))
-                .waitSeconds(1)
-                .lineToX(-34)
-                .waitSeconds(3)
-                .turn(Math.toRadians(45))
-                .strafeTo(new Vector2d(-12,-22))
-                .strafeTo(new Vector2d(-12,-32))
-                .strafeTo(new Vector2d(-12,-22))
-                .strafeTo(new Vector2d(-25,-12))
-                .turn(Math.toRadians(-50))
-                .lineToX(-34)
+        Action complex = drive.actionBuilder(beginPose)
+                .strafeTo(new Vector2d(58, 16))
+                .turn(Math.toRadians(-25))
+                .stopAndAdd(new functionOfDOOM())
+
+                .strafeToLinearHeading(new Vector2d(39, 28), Math.toRadians(90))
+                .stopAndAdd(new smartIntake())
+                .strafeTo(new Vector2d(39, 56))
+                .strafeTo(new Vector2d(39, 28))
+                .stopAndAdd(new stopSmartIntake())
+
+                .strafeToLinearHeading(new Vector2d(52, 16), Math.toRadians(135))
+                .stopAndAdd(new functionOfDOOM())
+
+                .strafeTo(new Vector2d(12, 28))
                 .build();
 
-        Action path3 = drive.actionBuilder(beginPose)
-
-                .lineToX(-25)
-                .turn(Math.toRadians(-130))
-                .waitSeconds(1)
-                .lineToX(-34)
-                .waitSeconds(3)
-                .turn(Math.toRadians(45))
-                .strafeTo(new Vector2d(-14,-22))
-                .strafeTo(new Vector2d(-14,-40))
-                .strafeTo(new Vector2d(-14,-22))
-                .strafeTo(new Vector2d(-52,-12))
-                .turn(Math.toRadians(-30))
-
-                .build();
-
-        Action path4 = drive.actionBuilder(beginPose)
-                .strafeToLinearHeading(new Vector2d(56,-16),Math.toRadians(-170))
-                .strafeTo(new Vector2d(35,-28))
-                .turn(Math.toRadians(80))
-                .strafeTo(new Vector2d(35,-52))
-                .strafeTo(new Vector2d(35,-28))
-                .strafeToLinearHeading(new Vector2d(56,-16),Math.toRadians(-170))
-                .strafeTo(new Vector2d(12,-28))
-                .turn(Math.toRadians(80))
-                .strafeTo(new Vector2d(12,-52))
-                .strafeTo(new Vector2d(12,-28))
-                .strafeToLinearHeading(new Vector2d(56,-16),Math.toRadians(-170))
-                .strafeTo(new Vector2d(-11,-28))
-                .turn(Math.toRadians(80))
-                .strafeTo(new Vector2d(-11,-52))
-                .strafeTo(new Vector2d(-11,-28))
-                .strafeToLinearHeading(new Vector2d(-34,-20),Math.toRadians(-120))
-                .strafeTo(new Vector2d(-50, -16))
-
-                .build();
-
-        Action Scrimmage2Auto = drive.actionBuilder(beginPose)
-
-                .strafeToLinearHeading(new Vector2d(56,16),Math.toRadians(170))
-                .strafeTo(new Vector2d(35,28))
-                .turn(Math.toRadians(-80))
-                .strafeTo(new Vector2d(35,52))
-                .strafeTo(new Vector2d(35,28))
-                .strafeToLinearHeading(new Vector2d(56,16),Math.toRadians(170))
-                .waitSeconds(1)
-                .strafeTo(new Vector2d(12,28))
-                .build();
-
-        Action Scrimmage2AutoWithShooting = drive.actionBuilder(beginPose)
-
-                .strafeToLinearHeading(new Vector2d(56,-16),Math.toRadians(-170))
-                .waitSeconds(0.5)
-                .strafeTo(new Vector2d(35,-28))
-                .turn(Math.toRadians(80))
-                .strafeTo(new Vector2d(35,-52))
-                .strafeTo(new Vector2d(35,-28))
-                .strafeToLinearHeading(new Vector2d(56,-16),Math.toRadians(-170))
-                .waitSeconds(0.5)
-                .strafeTo(new Vector2d(12,-28))
-                .build();
-
-
-
-        Actions.runBlocking(new SequentialAction(Scrimmage2Auto));
+        Actions.runBlocking(new SequentialAction(complex));
 
 
 
