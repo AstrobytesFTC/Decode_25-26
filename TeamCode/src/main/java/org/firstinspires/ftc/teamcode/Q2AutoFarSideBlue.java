45b7d85ea7b8d2da4b50e6388ad612986f9bae8c
sha580 <akshaya.vinodh711@gmail.com>
2026-01-04 17:52:31 -0500

    auto code but wrong

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Q2AutoFarSideBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Q2AutoFarSideBlue.java
@@ -15,6 +15,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
@@ -33,8 +34,8 @@ public class Q2AutoFarSideBlue extends LinearOpMode {
    // DcMotor launcher = null;
     DcMotor intake = null;
 
-    DcMotor shooterRight = null;
-    DcMotor shooterLeft = null;
+    DcMotorEx shooterRight = null;
+    DcMotorEx shooterLeft = null;
     Servo blocker = null;
 
     // lift class
@@ -59,7 +60,9 @@ public class Q2AutoFarSideBlue extends LinearOpMode {
 //            launcher.setPower(0);
 //        }
 //    }
-
+    public void sleepSeconds(double seconds) {
+    sleep((long)(seconds * 1000));
+    }
     public void timeTransferAndIntake(double seconds) {
         transfer.setPower(0.8);
         intake.setPower(-0.8);
@@ -67,43 +70,135 @@ public class Q2AutoFarSideBlue extends LinearOpMode {
         transfer.setPower(0);
         intake.setPower(0);
     }
-
     public class functionOfDOOM implements InstantFunction{
         @Override
         public void run(){
+
+            double doomVelocity = 1800;
+            double transfertime = 0.3;
+
+
+            boolean shootControl = false;
+
+            blocker.setPosition(0.1);
+            sleep(500);  // 500ms
             blocker.setPosition(0.5);
-            // === ULTIMATE FUNCTION OF DOOM ===
-            shooterRight.setPower(-0.90);
-            shooterLeft.setPower(0.90);
 
-// Spin-up time
-            sleep(2000);
+            shooterRight.setVelocity(1300);
+            shooterLeft.setVelocity(-1300);
+            transfer.setPower(-0.3);
+            intake.setPower(0.3);
 
-// === 1st Ball ===
-            timeTransferAndIntake(0.15);
-            sleep(1500);
+            sleep(500);
 
-// === 2nd Ball ===
-            timeTransferAndIntake(0.25);
-            sleep(1500);
+            shooterRight.setVelocity(0);
+            shooterLeft.setVelocity(0);
+            transfer.setPower(0);
+            intake.setPower(0);
 
-// === 3rd Ball ===
-            timeTransferAndIntake(0.35);
-            sleep(1500);
+            shooterRight.setVelocity(-doomVelocity);
+            shooterLeft.setVelocity(doomVelocity);
 
-// Power down shooter
-            shooterLeft.setPower(0);
-            shooterRight.setPower(0);
+            // Shoot 1
+            double currentVelocity = shooterLeft.getVelocity();
+            shootControl = true;
+            while (shootControl) {
+                if (Math.abs(currentVelocity - doomVelocity) < 40) {
+                    transfer.setPower(0.65);
+                    //intake.setPower(0.8);
+                    // new code
+                    sleepSeconds(transfertime - 0.15);
+                    transfer.setPower(-0.4);
+                    sleepSeconds(transfertime - 0.15);
+                    transfer.setPower(0);
+                    shootControl = false;
+                    telemetry.addLine("Shot 1 completed");
+                    telemetry.update();
+//                double doomTimer = System.currentTimeMillis();
 
-            blocker.setPosition(0);
+                }
+            }
+            // Shoot 2
+            shootControl = true;
+            currentVelocity = shooterLeft.getVelocity();
+            while(shootControl) {
+                if (Math.abs(currentVelocity - doomVelocity) < 40) {
+                    transfer.setPower(0.65);
+                    sleepSeconds(transfertime);
+                    transfer.setPower(0);
+                    shootControl = false;
+                    telemetry.addLine("Shot 2 completed");
+                    telemetry.update();
+                    //intake.setPower(0.8);
+//                double doomTimer = System.currentTimeMillis();
+//                doomStep++;
+                }
+            }
+
+            // Shoot 3
+            shootControl = true;
+            currentVelocity = shooterLeft.getVelocity();
+            while(shootControl) {
+                if (Math.abs(currentVelocity - doomVelocity) < 40) {
+                    transfer.setPower(0.7);
+                    intake.setPower(0.7);
+                    sleepSeconds(transfertime+0.1);
+                    transfer.setPower(0);
+                    shootControl = false;
+                    telemetry.addLine("Shot 3 completed");
+                    telemetry.update();
+                    //intake.setPower(0.8);
+//                double doomTimer = System.currentTimeMillis();
+//                doomStep++;
+                }
+            }
+            doomVelocity = 0;
+            shooterRight.setVelocity(0);
+            shooterLeft.setVelocity(0);
+            intake.setPower(0);
         }
     }
+
+
+
+//    public class functionOfDOOM implements InstantFunction{
+//        @Override
+//        public void run(){
+//            blocker.setPosition(0.5);
+//            // === ULTIMATE FUNCTION OF DOOM === <has to be replaced>
+//            shooterRight.setPower(-0.90);
+//            shooterLeft.setPower(0.90);
+//
+//// Spin-up time
+//            sleep(2000);
+//
+//// === 1st Ball ===
+//            timeTransferAndIntake(0.15);
+//            sleep(1500);
+//
+//// === 2nd Ball ===
+//            timeTransferAndIntake(0.25);
+//            sleep(1500);
+//
+//// === 3rd Ball ===
+//            timeTransferAndIntake(0.35);
+//            sleep(1500);
+//
+//// Power down shooter
+//            shooterLeft.setPower(0);
+//            shooterRight.setPower(0);
+//
+//            blocker.setPosition(0);
+//        }
+//    }
     public class smartIntake implements InstantFunction{
         @Override
         public void run(){
             //Change If needed
-            intake.setPower(-0.9);
-            transfer.setPower(0.8);
+            transfer.setPower(0.6);
+            intake.setPower(0.75);
+            shooterRight.setVelocity(-1450);
+            shooterLeft.setVelocity(1450);
         }
     }
     public class stopSmartIntake implements InstantFunction{
@@ -112,6 +207,8 @@ public class Q2AutoFarSideBlue extends LinearOpMode {
             //Change If needed
             intake.setPower(0);
             transfer.setPower(0);
+            shooterRight.setVelocity(0);
+            shooterLeft.setVelocity(0);
         }
     }
 
@@ -126,8 +223,8 @@ public class Q2AutoFarSideBlue extends LinearOpMode {
         backRightMotor  = hardwareMap.dcMotor.get("backright");
         transfer = hardwareMap.dcMotor.get("intake");
         intake = hardwareMap.dcMotor.get("transfer");
-        shooterLeft = hardwareMap.dcMotor.get("rightShooter");
-        shooterRight = hardwareMap.dcMotor.get("leftShooter");
+        shooterLeft  = hardwareMap.get(DcMotorEx.class, "leftShooter");
+        shooterRight = hardwareMap.get(DcMotorEx.class, "rightShooter");
         blocker = hardwareMap.servo.get("blocker");
 
         Pose2d beginPose = new Pose2d(new Vector2d(65,-16), Math.toRadians(-170));
@@ -219,6 +316,7 @@ public class Q2AutoFarSideBlue extends LinearOpMode {
         Action Scrimmage2Auto = drive.actionBuilder(beginPose)
 
                 .strafeTo(new Vector2d(58,-16))
+
                 .turn(Math.toRadians(20))
                 .stopAndAdd(new functionOfDOOM() )
 
