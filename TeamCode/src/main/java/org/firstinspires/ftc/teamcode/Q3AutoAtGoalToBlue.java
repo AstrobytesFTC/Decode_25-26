6e68a1411c0a0e9daea33100056e0390a5c0e1dc
sha580 <akshaya.vinodh711@gmail.com>
2026-01-20 18:23:53 -0500

    q3 at goal blue needs fixing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Q3AutoAtGoalToBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Q3AutoAtGoalToBlue.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 
 // RR-specific imports
+import static java.lang.Thread.sleep;
 
 import com.acmerobotics.dashboard.config.Config;
 
@@ -39,28 +40,143 @@ public class Q3AutoAtGoalToBlue extends LinearOpMode {
     DcMotorEx shooterLeft = null;
     Servo blocker = null;
 
+    double velocityPowerFar = 1700;
+    double velocityPowerNear = 1450;
     // lift class
     private boolean initialized = false;
 
-//    public class warmupLaunch implements InstantFunction{
-//        @Override
-//        public void run(){
-//            launcher.setPower(-0.8);
-//        }
-//    }
+    public class runShooter implements InstantFunction{
+        @Override
+        public void run(){
+            shooterLeft.setVelocity(-1420);
+            shooterRight.setVelocity(1420);
+        }
 
-    //    public class reverselaunch implements InstantFunction{
-//        @Override
-//        public void run(){
-//            launcher.setPower(0.5);
-//        }
-//    }
-//    public class stopLauncher implements InstantFunction{
-//        @Override
-//        public void run(){
-//            launcher.setPower(0);
-//        }
-//    }
+    }
+
+    public class reduceShooterSpeed implements InstantFunction{
+        @Override
+        public void run(){
+            shooterLeft.setVelocity(-velocityPowerNear);
+            shooterRight.setVelocity(velocityPowerNear);
+        }
+
+    }
+    public class increaseShooterSpeed implements InstantFunction{
+        @Override
+        public void run(){
+            shooterLeft.setVelocity(-velocityPowerFar);
+            shooterRight.setVelocity(velocityPowerFar);
+        }
+    }
+    public class blockerUp implements InstantFunction{
+        @Override
+        public void run(){
+            blocker.setPosition(0.5);
+        }
+
+    }
+    public class blockerDown implements InstantFunction {
+        @Override
+        public void run() {
+            blocker.setPosition(0.1);
+        }
+
+    }
+    public class smartFeed implements InstantFunction{
+        @Override
+        public void run(){
+            transfer.setPower(0.8);
+            intake.setPower(0.8);
+
+            sleepSeconds(.5);
+
+            transfer.setPower(0);
+            intake.setPower(0);
+        }
+
+    }
+    public class smartIntake implements InstantFunction {
+        @Override
+        public void run() {
+            intake.setPower(0.8);
+            transfer.setPower(0.8);
+        }
+
+    }
+    public class stopSmartIntake implements InstantFunction{
+        @Override
+        public void run(){
+            intake.setPower(0);
+            transfer.setPower(0);
+        }
+
+    }
+
+
+    public boolean waitForShooter(DcMotorEx shooter, double target, long timeoutMs) {
+        long start = System.currentTimeMillis();
+
+        while (opModeIsActive()
+                && System.currentTimeMillis() - start < timeoutMs) {
+
+            double velocity = Math.abs(shooter.getVelocity());
+//Dependable, keep if close
+            if (Math.abs(velocity - target) < 40) {
+                return true;
+            }
+
+            sleep(5); // allow hardware loop
+        }
+        return false; // timed out
+    }
+
+
+
+    public class smartFeedNear implements InstantFunction{
+        @Override
+        public void run(){
+            blocker.setPosition(0.5);
+            //telemetry.addLine("NOT DONE");
+            if(waitForShooter(shooterLeft, velocityPowerNear,3000)){
+                //telemetry.addLine("SHOT");
+                transfer.setPower(0.8);
+                intake.setPower(0.8);
+
+                sleepSeconds(.5);
+
+                transfer.setPower(0);
+                intake.setPower(0);
+            }
+            //telemetry.addLine("DONEE");
+            blocker.setPosition(0.1);
+            //telemetry.update();
+        }
+
+    }
+
+    public class smartFeedFar implements InstantFunction{
+        @Override
+        public void run(){
+            blocker.setPosition(0.5);
+            //telemetry.addLine("NOT DONE");
+            if(waitForShooter(shooterLeft, velocityPowerFar,3000)){
+                //  telemetry.addLine("SHOT");
+                transfer.setPower(0.8);
+                intake.setPower(0.8);
+
+                sleepSeconds(.5);
+
+                transfer.setPower(0);
+                intake.setPower(0);
+            }
+            // telemetry.addLine("DONEE");
+            blocker.setPosition(0.1);
+            //telemetry.update();
+        }
+
+    }
+
     public void sleepSeconds(double seconds) {
         sleep((long)(seconds * 1000));
     }
@@ -71,223 +187,31 @@ public class Q3AutoAtGoalToBlue extends LinearOpMode {
         transfer.setPower(0);
         intake.setPower(0);
     }
-    public boolean waitForShooter(DcMotorEx shooter, double target, long timeoutMs) {
-        long start = System.currentTimeMillis();
 
-        while (opModeIsActive()
-                && System.currentTimeMillis() - start < timeoutMs) {
-
-            double velocity = Math.abs(shooter.getVelocity());
-//Dependable, keep if close
-            if (Math.abs(velocity - target) < 30) {
-                return true;
-            }
-
-            sleep(10); // allow hardware loop
-        }
-        return false; // timed out
-    }
-
-    public class functionOfDOOM implements InstantFunction{
-        @Override
-        public void run(){
-            boolean shootControl = false;
-            double doomVelocity = 1400;
-            double transfertime = 0.3;
-
-            blocker.setPosition(0.1);
-            sleep(1000);  // 500ms
-            blocker.setPosition(0.5);
-
-            shooterRight.setVelocity(1300);
-            shooterLeft.setVelocity(-1300);
-            transfer.setPower(-0.3);
-            intake.setPower(0.3);
-
-            sleep(500);
-
-            shooterRight.setVelocity(0);
-            shooterLeft.setVelocity(0);
-            transfer.setPower(0);
-            intake.setPower(0);
-
-            shooterRight.setVelocity(doomVelocity);
-            shooterLeft.setVelocity(-doomVelocity);
-
-            intake.setPower(0.3);
-
-            if (waitForShooter(shooterRight, doomVelocity, 1500)) {
-                transfer.setPower(0.65);
-                sleepSeconds(transfertime - 0.15);
-                transfer.setPower(-0.4);
-                sleepSeconds(transfertime - 0.15);
-                transfer.setPower(0);
-            }
-
-            if (waitForShooter(shooterLeft, doomVelocity, 4000)) {
-                transfer.setPower(0.65);
-                intake.setPower(0.2);
-                sleepSeconds(transfertime);
-//                transfer.setPower(-0.4);
-//                sleepSeconds(transfertime - 0.15);
-                transfer.setPower(0);
-                intake.setPower(0.3);
-            }
-
-            if (waitForShooter(shooterLeft, doomVelocity, 4000)) {
-                transfer.setPower(1);
-                intake.setPower(0.7);
-                sleepSeconds(transfertime + 0.1);
-                transfer.setPower(0);
-            }
-            doomVelocity = 0;
-            shooterRight.setVelocity(-200);
-            shooterLeft.setVelocity(200);
-            sleepSeconds(0.1);
-            shooterRight.setVelocity(0);
-            shooterLeft.setVelocity(0);
-            intake.setPower(0);
-        }
-    }
-    public class fasterfunctionOfDOOM implements InstantFunction{
-        @Override
-        public void run(){
-            boolean shootControl = false;
-            double doomVelocity = 1400;
-            double transfertime = 0.3;
-
-            blocker.setPosition(0.1);
-            sleep(1000);  // 500ms
-            blocker.setPosition(0.5);
-
-            shooterRight.setVelocity(1300);
-            shooterLeft.setVelocity(-1300);
-            transfer.setPower(-0.3);
-            intake.setPower(0.3);
-
-            sleep(500);
-
-            shooterRight.setVelocity(0);
-            shooterLeft.setVelocity(0);
-            transfer.setPower(0);
-            intake.setPower(0);
-
-            shooterRight.setVelocity(doomVelocity);
-            shooterLeft.setVelocity(-doomVelocity);
-
-            intake.setPower(0.3);
-
-            if (waitForShooter(shooterRight, doomVelocity, 1500)) {
-                transfer.setPower(0.65);
-                sleepSeconds(transfertime - 0.15);
-                transfer.setPower(-0.4);
-                sleepSeconds(transfertime - 0.15);
-                transfer.setPower(0);
-            }
-
-            if (waitForShooter(shooterLeft, doomVelocity, 4000)) {
-                transfer.setPower(0.65);
-                intake.setPower(0.2);
-                sleepSeconds(transfertime);
-//                transfer.setPower(-0.4);
-//                sleepSeconds(transfertime - 0.15);
-                transfer.setPower(0);
-                intake.setPower(0.3);
-            }
-
-            if (waitForShooter(shooterLeft, doomVelocity, 4000)) {
-                transfer.setPower(1);
-                intake.setPower(0.7);
-                sleepSeconds(transfertime + 0.1);
-                transfer.setPower(0);
-            }
-            doomVelocity = 0;
-            shooterRight.setVelocity(-200);
-            shooterLeft.setVelocity(200);
-            sleepSeconds(0.1);
-            shooterRight.setVelocity(0);
-            shooterLeft.setVelocity(0);
-            intake.setPower(0);
-        }
-    }
-
-
-
-//    public class functionOfDOOM implements InstantFunction{
+//    public class stopLauncher implements InstantFunction {
 //        @Override
-//        public void run(){
-//            blocker.setPosition(0.5);
-//            // === ULTIMATE FUNCTION OF DOOM === <has to be replaced>
-//            shooterRight.setPower(-0.90);
-//            shooterLeft.setPower(0.90);
-//
-//// Spin-up time
-//            sleep(2000);
-//
-//// === 1st Ball ===
-//            timeTransferAndIntake(0.15);
-//            sleep(1500);
-//
-//// === 2nd Ball ===
-//            timeTransferAndIntake(0.25);
-//            sleep(1500);
-//
-    //// === 3rd Ball ===
-//            timeTransferAndIntake(0.35);
-//            sleep(1500);
-//
-//    // Power down shooter
-//            shooterLeft.setPower(0);
-//            shooterRight.setPower(0);
-//
-//            blocker.setPosition(0);
+//        public void run() {
+//            //Change If needed
+//            //shooterRight.setVelocity(-300);
+//            //shooterLeft.setVelocity(-300);
+//            //sleepSeconds(0.2);
+//            shooterRight.setVelocity(-300);
+//            shooterLeft.setVelocity(300);
 //        }
 //    }
-    public class smartIntake implements InstantFunction{
-        @Override
-        public void run(){
-            //Change If needed
-            transfer.setPower(0.6);
-            intake.setPower(0.75);
-            shooterRight.setVelocity(-1450);
-            shooterLeft.setVelocity(1450);
-        }
-    }
-    public class stopSmartIntake implements InstantFunction{
-        @Override
-        public void run(){
-            //Change If needed
-            intake.setPower(0);
-            transfer.setPower(0);
-            shooterRight.setVelocity(0);
-            shooterLeft.setVelocity(0);
-        }
-    }
-
-    public class stopLauncher implements InstantFunction {
-        @Override
-        public void run() {
-            //Change If needed
-            //shooterRight.setVelocity(-300);
-            //shooterLeft.setVelocity(-300);
-            //sleepSeconds(0.2);
-            shooterRight.setVelocity(-300);
-            shooterLeft.setVelocity(300);
-        }
-    }
-
-    public class stopLauncher1 implements InstantFunction{
-        @Override
-        public void run(){
-            //Change If needed
-            //shooterRight.setVelocity(-300);
-            //shooterLeft.setVelocity(-300);
-            //sleepSeconds(0.2);
-            shooterRight.setVelocity(0);
-            shooterLeft.setVelocity(0);
-
-        }
-    }
+//
+//    public class stopLauncher1 implements InstantFunction{
+//        @Override
+//        public void run(){
+//            //Change If needed
+//            //shooterRight.setVelocity(-300);
+//            //shooterLeft.setVelocity(-300);
+//            //sleepSeconds(0.2);
+//            shooterRight.setVelocity(0);
+//            shooterLeft.setVelocity(0);
+//
+//        }
+//    }
 
 
     public void runOpMode() {
@@ -319,42 +243,53 @@ public class Q3AutoAtGoalToBlue extends LinearOpMode {
         waitForStart();
 
         Action Scrimmage2Auto = drive.actionBuilder(beginPose)
+                .stopAndAdd(new runShooter())
                 .strafeToLinearHeading(new Vector2d(-23,-27), Math.toRadians(-135))
+                .stopAndAdd(new smartFeedNear())
                 //goes to intake artifacts row 1
 
                 .strafeToLinearHeading(new Vector2d(-15,-40), Math.toRadians(-90))
-
+                .stopAndAdd(new smartIntake())
 //intakes artifacts
 
-                .strafeTo(new Vector2d(-15,-62))
+                .strafeTo(new Vector2d(-15,-66))
                 .strafeTo(new Vector2d(-15,-40))
+                .stopAndAdd(new stopSmartIntake())
 
 //goes to shooting position
-
+//                .stopAndAdd(new runShooter())
                 .strafeToLinearHeading(new Vector2d(-23,-27), Math.toRadians(-135))
+                .stopAndAdd(new smartFeedNear())
                 //goes to intake second row of artifacts
 
-                .strafeToLinearHeading(new Vector2d(10,-28), Math.toRadians(-90))
+                .strafeToLinearHeading(new Vector2d(10,-30), Math.toRadians(-90))
+                .stopAndAdd(new smartIntake())
 
 //intakes artifacts row 2
 
                 .strafeTo(new Vector2d(10,-62))
-                .strafeTo(new Vector2d(10,-28))
+                .strafeTo(new Vector2d(10,-30))
+                .stopAndAdd(new stopSmartIntake())
 
 //goes to shooting position
 
                 .strafeToLinearHeading(new Vector2d(-23,-27), Math.toRadians(-135))
+                .stopAndAdd(new smartFeedNear())
+                .stopAndAdd(new increaseShooterSpeed())
 
                 //goes to intake third row of artifacts
                 .strafeToLinearHeading(new Vector2d(35,-52), Math.toRadians(-90))
+                .stopAndAdd(new smartIntake())
                 //intakes artifacts
 
                 .strafeTo(new Vector2d(35,-65))
                 .strafeTo(new Vector2d(35,-52))
+                .stopAndAdd(new stopSmartIntake())
 
 //goes to shooting position
 
-                .strafeToLinearHeading(new Vector2d(57,-35),Math.toRadians(-165))
+                .strafeToLinearHeading(new Vector2d(62,-33),Math.toRadians(-160))
+                .stopAndAdd(new smartFeedFar())
                 //shoots
                 .strafeTo(new Vector2d(38,-25))
 
