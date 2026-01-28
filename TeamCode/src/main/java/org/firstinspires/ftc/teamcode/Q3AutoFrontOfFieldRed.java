7ed29aa6d2259f2e3d5a92132f549aa9c7d8cd8c
Lohith-Sharan <lohithsharanm@gmail.com>
2026-01-28 16:00:50 -0500

    Updated Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Q3AutoFrontOfFieldRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Q3AutoFrontOfFieldRed.java
@@ -42,7 +42,7 @@ public class Q3AutoFrontOfFieldRed extends LinearOpMode {
     Servo blocker = null;
     //    double velocityPower = 1880;
 
-    double velocityPowerFar = 1680;
+    double velocityPowerFar = 1670;
     double velocityPowerNear = 1430;
 
 
@@ -243,7 +243,7 @@ public class Q3AutoFrontOfFieldRed extends LinearOpMode {
 
                 //shooting position
                 .stopAndAdd(new runShooter())
-                .strafeToLinearHeading(new Vector2d(56, 14), Math.toRadians(150))
+                .strafeToLinearHeading(new Vector2d(53, 14), Math.toRadians(150))
                 .stopAndAdd(new smartFeedFar())
 
                 //goes to intake position
