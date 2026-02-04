bd5b57f87335f8b497618d2331bbede6760c5c19
Mugilshark <94998254+Mugilshark@users.noreply.github.com>
2026-02-03 19:23:07 -0500

    Rename Change rq

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StatesAtGoalToBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StatesAtGoalToBlue.java
@@ -22,14 +22,10 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 
 @Config
-<<<<<<< Updated upstream:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StatesAtGoalToBlue.java
-@Autonomous(group = "Autonomous")
-public class StatesAtGoalToBlue extends LinearOpMode {
-=======
-@Autonomous(name = "StatesAutoAtGoalBlue", group = "Autonomous")
-public class StatesAutoAtGoalBlue extends LinearOpMode {
->>>>>>> Stashed changes:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StatesAutoAtGoalBlue.java
 
+
+@Autonomous(name = "StatesAutoAtGoalBlue", group = "Autonomous")
+public class StatesAtGoalToBlue extends LinearOpMode {
 
     DcMotor frontLeftMotor = null;
     DcMotor backLeftMotor = null;
